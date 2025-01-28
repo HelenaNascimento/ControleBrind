6747350fedeca86b2e3e5d4340c56a897a75feb6
Helena Nascimento <hnasc.1989@gmail.com>
2025-01-28 17:31:12 -0300

    'atualizando o arquivo de entrada'

--- SCRIPT_BD/LOAD/PR_CadastrarFornecedor .sql
+++ SCRIPT_BD/LOAD/PR_CadastrarFornecedor .sql
@@ -1,16 +1,23 @@
+USE BD_BRIND
+GO
+SET ANSI_NULLS, QUOTED_IDENTIFIER ON
+GO
 
-CREATE PROCEDURE PR_CadastrarFornecedor 
+ALTER PROCEDURE PR_CadastrarFornecedor 
 	(	@Razao_Social  Varchar(80),
 		@CNPJ VARCHAR(14),
 		@FLG_COMPRA BIT,
-		@SITE VARCHAR(80), 
+		@SITE VARCHAR(80) null, 
 		@IdBair int, 
 		@IdCid int, 
 		@IdEst int, 
-		@UF VARCHAR(2),
-		@Rua varchar(80), 
-		@Numero int, 
-		@Complemento varchar(80))
+		@IdForne int,
+		@Cidade varchar(80) null,
+		@Bairro varchar (80) null,
+		@UF VARCHAR(2) null,
+		@Rua varchar(80) null, 
+		@Numero int null, 
+		@Complemento varchar(80) null)
 
 WITH ENCRYPTION
 AS
@@ -23,36 +30,58 @@ IF(select top 1 1 from FORNE where CNPJ_FORN = @CNPJ) = 1
 ELSE INSERT FORNE(Razao_Social, CNPJ_FORN, Tip_Comp)
 		VALUES (@Razao_Social, @CNPJ, @FLG_COMPRA)
 
-	IF @FLG_COMPRA = 1
-		
-		EXEC TR_CadWeb_Fornecedor @SITE
+	SET @FLG_COMPRA = IsNull(@FLG_COMPRA, 0)
 
 	IF @FLG_COMPRA = 0
 
-		DECLARE C_idEnt CURSOR FOR
+		DECLARE C_Forne CURSOR FOR
+			SELECT
+			ID_FORNE = (Select max(ID_FORNE) from FORNE)
+		OPEN C_Forne
+		FETCH NEXT FROM C_Forne INTO @IdForne
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+
+			INSERT INTO WEB (SITE, ID_FORNE)
+			VALUES (@SITE, @IdForne)
+			FETCH NEXT FROM C_Forne INTO @IdForne
+		END;
+
+		CLOSE C_Forne
+		DEALLOCATE C_Forne
+
+	IF @FLG_COMPRA = 1
+
+		DECLARE C_Forne CURSOR FOR
 			SELECT 
 				BR.ID_BAIRRO,
 				CD.ID_CIDADE,
-				ES.ID_ESTADO
+				ES.ID_ESTADO,
+				ID_FORNE = (Select max(ID_FORNE) from FORNE)
 			FROM ESTADO ES
 				LEFT JOIN CIDADE CD ON ES.ID_ESTADO = CD.ID_ESTADO
 				LEFT JOIN BAIRRO BR ON CD.ID_CIDADE = BR.ID_CIDADE
 			WHERE ES.UF = @UF
+			AND CD.Nome_Cidade = @Cidade
+			AND BR.Nome_Bairro = @Bairro
 
-		OPEN C_idEnt
-		FETCH NEXT FROM CursorProd INTO @IdBair, @IdCid, @IdEst
+
+		OPEN C_Forne
+		FETCH NEXT FROM C_Forne INTO @IdBair, @IdCid, @IdEst, @IdForne
 		WHILE @@FETCH_STATUS = 0
 		BEGIN
-			INSERT INTO LOGRAD (BR.ID_BAIRRO, CD.ID_CIDADE, ES.ID_ESTADO, RUA, NUMER, Complemento)
-			VALUES (@IdBair, @IdCid, @IdEst, @Rua, @Numero, @Complemento)
+			INSERT INTO LOGRAD (BR.ID_BAIRRO, CD.ID_CIDADE, ES.ID_ESTADO, RUA, NUMER, Complemento, ID_FORNE)
+			VALUES (@IdBair, @IdCid, @IdEst, @Rua, @Numero, @Complemento, @IdForne)
 
-			FETCH NEXT FROM CursorProd INTO @IdBair, @IdCid, @IdEst
+			FETCH NEXT FROM C_Forne INTO @IdBair, @IdCid, @IdEst, @IdForne
 		END;
 
-		CLOSE C_idEnt
-		DEALLOCATE C_idEnt
+		CLOSE C_Forne
+		DEALLOCATE C_Forne
 
 		
 		
 		
 	
+GO
+
